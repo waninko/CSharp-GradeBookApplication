1b65ef7d6e05fa10a3631c001804e486bb2cd17f
waninko <43968374+waninko@users.noreply.github.com>
2019-04-04 12:18:23 +0200

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,32 +16,23 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
             {
-               throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
-                /* var threshold = (int)Math.Ceiling(Students.Count * 0, 2);
-                 var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
-
-                 if (grades[threshold - 1] <= averageGrade)
-                     return 'A';
-                 else if (grades[(threshold * 2) - 1] <= averageGrade)
-                     return 'B';
-                 else if (grades[(threshold * 3) - 1] <= averageGrade)
-                     return 'C';
-                 else if (grades[(threshold * 4) - 1] <= averageGrade)
-                     return 'D';
-                 else return 'F';*/
-
-                if (averageGrade >= 90)
-                    return 'A';
-                else if (averageGrade >= 80)
-                    return 'B';
-                else if (averageGrade >= 70)
-                    return 'C';
-                else if (averageGrade >= 60)
-                    return 'D';
-                else
-                    return 'F';
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+               
             }
-
+            var test = Students.Count * (0.2);
+            var threshold = (int)Math.Ceiling(test);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            if(Students.Count >= 5){
+                if (grades[threshold - 1] <= averageGrade)
+                    return 'A';
+                else if (grades[(threshold * 2) - 1] <= averageGrade)
+                    return 'B';
+                else if (grades[(threshold * 3) - 1] <= averageGrade)
+                    return 'C';
+                else if (grades[(threshold * 4) - 1] <= averageGrade)
+                    return 'D';
+                else return 'F';
+            }
             return base.GetLetterGrade(averageGrade);
         }
     }
