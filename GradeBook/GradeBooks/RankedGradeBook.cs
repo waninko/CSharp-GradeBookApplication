dda6db15cd300524f3f9dc0eaefa0c30ee8e9f77
waninko <43968374+waninko@users.noreply.github.com>
2019-04-04 12:11:59 +0200

    trying getGrade with if statements

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -17,18 +17,29 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
             {
                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
-                var threshold = (int)Math.Ceiling(Students.Count * 0, 2);
-                var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+                /* var threshold = (int)Math.Ceiling(Students.Count * 0, 2);
+                 var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-                if (grades[threshold - 1] <= averageGrade)
+                 if (grades[threshold - 1] <= averageGrade)
+                     return 'A';
+                 else if (grades[(threshold * 2) - 1] <= averageGrade)
+                     return 'B';
+                 else if (grades[(threshold * 3) - 1] <= averageGrade)
+                     return 'C';
+                 else if (grades[(threshold * 4) - 1] <= averageGrade)
+                     return 'D';
+                 else return 'F';*/
+
+                if (averageGrade >= 90)
                     return 'A';
-                else if (grades[(threshold * 2) - 1] <= averageGrade)
+                else if (averageGrade >= 80)
                     return 'B';
-                else if (grades[(threshold * 3) - 1] <= averageGrade)
+                else if (averageGrade >= 70)
                     return 'C';
-                else if (grades[(threshold * 4) - 1] <= averageGrade)
+                else if (averageGrade >= 60)
                     return 'D';
-                else return 'F';
+                else
+                    return 'F';
             }
 
             return base.GetLetterGrade(averageGrade);
